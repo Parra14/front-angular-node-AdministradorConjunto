85e6d627e79d9a538f3c01f21203b58e3ff035d1
Parra14 <ofparra69@gmail.com>
2024-04-29 18:39:00 -0500

    *

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+//components
+import {HeaderComponent} from './shaders/header/header.component'
+import {ListReservationComponent} from './components/list-reservation/list-reservation.component'
+import {CreateReservationComponent} from './components/create-reservation/create-reservation.component'
+
+
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'list-reservation',
+        pathMatch: 'full'
+    },
+    {
+        path: 'list-reservation',
+        component: ListReservationComponent
+    }
+];
