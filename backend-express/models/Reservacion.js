84762edb9ed938d103aef220599a8e65c41e14d6
JalexanderMurallas <jalexandermurallas@uts.edu.co>
2024-06-07 17:53:47 -0500

    Update Reservation.js

--- backend-express/models/Reservacion.js
+++ backend-express/models/Reservacion.js
@@ -17,7 +17,28 @@ const ReservacionSchema = new Schema(
       required: [true, "Campo requerido"],
       uppercase: true,
     },
-    fechaReserva: Date,
+    fechaReserva: {
+        type: String,
+        required: [true, "Campo requerido"],
+        validate: {
+          validator: function(v) {
+            // This regex checks for the format dd/mm/yyyy
+            return /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/(19|20)\d{2}$/.test(v);
+          },
+          message: props => `${props.value} no es una fecha válida. El formato correcto es DD/MM/AAAA`
+        }
+      },
+    horaReserva: {
+        type: String,
+        required: [true, "Campo requerido"],
+        validate: {
+          validator: function(v) {
+            // This regex checks if the time is in HH:MM format and in 30-minute intervals
+            return /^([01]?[0-9]|2[0-3]):([03]0)$/.test(v);
+          },
+          message: props => `${props.value} no es un intervalo válido de 30 minutos`
+        }
+      },
   },
   {
     timestamps: true,
